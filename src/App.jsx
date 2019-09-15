8a0f23ee3e5d3936bb75e4cf83dea695ab378a51
git <marlonergondossantos@gmail.com>
2019-09-15 10:38:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Configuracao do projeto funcionando</h1>
+      </>
+    );
+  }
+}
+
+export default App;
