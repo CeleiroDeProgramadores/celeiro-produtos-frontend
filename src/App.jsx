a2b02cc166085b553da92a09e3228e02c9881e25
git <marlonergondossantos@gmail.com>
2019-09-15 21:03:03 -0300

    organizando arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-
+import GlobalStyled from "./globalStyle";
 class App extends Component {
   render() {
     return (
       <>
+        <GlobalStyled />
         <h1>Configuracao do projeto funcionando</h1>
       </>
     );
